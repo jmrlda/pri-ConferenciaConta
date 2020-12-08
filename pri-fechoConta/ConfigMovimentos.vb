ff94f93b626ce575e1dc761c11088211961d0c63
liry.linx <dercioguirruta44@gmail.com>
2020-12-08 12:08:29 +0200

    #17 - Carregar e Configurar os movimentos e associar os codigos a suas descrições
    #

--- pri-fechoConta/ConfigMovimentos.vb
+++ pri-fechoConta/ConfigMovimentos.vb
@@ -29,94 +29,148 @@ Public Class ConfigMovimentos
     Private Sub chkLstMovCodigo_ItemCheck(sender As Object, e As ItemCheckEventArgs) Handles chkLstMovCodigo.ItemCheck
 
         If Not loading Then
-            Dim i As Integer = Me.chkLstMovCodigo.SelectedIndex()
+            'Dim i As Integer = Me.chkLstMovCodigo.SelectedIndex()
+            Dim i As Integer = e.Index 'Me.chkLstMovDescricao.SelectedIndex()
 
             Dim codigo As String
             codigo = Me.chkLstMovCodigo.Items.Item(i).ToString
+
+
 
 
             If Not (Me.chkLstMovCodigo.GetItemChecked(i)) Then
                 ' Adicionar codigo que representa cada movimento a uma lista 
 
                 If rdBtnNumerario.Checked Then
-                    numerario_mov.codigo.Add(codigo)
+                    If Not numerario_mov.codigo.Contains(codigo) Then
+
+                        numerario_mov.codigo.Add(codigo)
+                    End If
                     numerario_mov.tipoMovimento = "numerario"
 
                 ElseIf rdBtnCheques.Checked Then
-                    cheque_mov.codigo.Add(codigo)
+                    If Not cheque_mov.codigo.Contains(codigo) Then
+
+                        cheque_mov.codigo.Add(codigo)
+                    End If
                     cheque_mov.tipoMovimento = "cheque"
 
 
                 ElseIf rdBtnPagAutomatico.Checked Then
-                    pagamentoAutomatico_mov.codigo.Add(codigo)
+                    If Not pagamentoAutomatico_mov.codigo.Contains(codigo) Then
+
+                        pagamentoAutomatico_mov.codigo.Add(codigo)
+                    End If
                     pagamentoAutomatico_mov.tipoMovimento = "pagamento automatico"
 
                 ElseIf rdBtnOutros.Checked Then
-                    outros_mov.codigo.Add(codigo)
+                    If Not outros_mov.codigo.Contains(codigo) Then
+
+                        outros_mov.codigo.Add(codigo)
+                    End If
                     outros_mov.tipoMovimento = "outros"
                 ElseIf rdBtnAbertura.Checked Then
-                    abertura_mov.codigo.Add(codigo)
+                    If Not abertura_mov.codigo.Contains(codigo) Then
+
+                        abertura_mov.codigo.Add(codigo)
+                    End If
                     abertura_mov.tipoMovimento = "abertura caixa"
                 ElseIf rdBtnFecho.Checked Then
-                    fecho_mov.codigo.Add(codigo)
+                    If Not fecho_mov.codigo.Contains(codigo) Then
+
+                        fecho_mov.codigo.Add(codigo)
+                    End If
                     fecho_mov.tipoMovimento = "fecho caixa"
                 ElseIf rdBtnEntrada.Checked Then
-                    entrada_mov.codigo.Add(codigo)
+                    If Not entrada_mov.codigo.Contains(codigo) Then
+
+                        entrada_mov.codigo.Add(codigo)
+                    End If
                     entrada_mov.tipoMovimento = "entrada caixa"
+                ElseIf rdBtnSaida.Checked Then
+                    If Not saida_mov.codigo.Contains(codigo) Then
+                        saida_mov.codigo.Add(codigo)
+                    End If
+                    saida_mov.tipoMovimento = "saida caixa"
                 End If
-            ElseIf rdBtnSaida.Checked Then
-                saida_mov.codigo.Add(codigo)
-                saida_mov.tipoMovimento = "saida caixa"
 
             Else
-                ' Remover codigo da lista de  cada movimento 
-                Dim item_idx As Integer
-                If rdBtnNumerario.Checked Then
-                    item_idx = numerario_mov.codigo.IndexOf(codigo)
-                    If item_idx >= 0 Then
-                        numerario_mov.codigo.RemoveAt(item_idx)
-                    End If
-                ElseIf rdBtnCheques.Checked Then
-                    item_idx = cheque_mov.codigo.IndexOf(codigo)
-                    If item_idx >= 0 Then
-                        cheque_mov.codigo.RemoveAt(item_idx)
-                    End If
-                ElseIf rdBtnPagAutomatico.Checked Then
-                    item_idx = pagamentoAutomatico_mov.codigo.IndexOf(codigo)
-                    If item_idx >= 0 Then
-                        pagamentoAutomatico_mov.codigo.RemoveAt(item_idx)
-                    End If
-                ElseIf rdBtnOutros.Checked Then
-                    item_idx = outros_mov.codigo.IndexOf(codigo)
-                    If item_idx >= 0 Then
-                        outros_mov.codigo.RemoveAt(item_idx)
-                    End If
-                ElseIf rdBtnAbertura.Checked Then
-                    item_idx = abertura_mov.codigo.IndexOf(codigo)
-                    If item_idx >= 0 Then
-                        abertura_mov.codigo.RemoveAt(item_idx)
-                    End If
-                ElseIf rdBtnFecho.Checked Then
-                    item_idx = fecho_mov.codigo.IndexOf(codigo)
-                    If item_idx >= 0 Then
-                        fecho_mov.codigo.RemoveAt(item_idx)
-                    End If
-                ElseIf rdBtnEntrada.Checked Then
-                    item_idx = entrada_mov.codigo.IndexOf(codigo)
-                    If item_idx >= 0 Then
+
+
+
+
+
+                    'item_idx = numerario_mov.descricao.IndexOf(descricao)
+
+                    'While item_idx >= 0
+                    '    numerario_mov.descricao.RemoveAt(item_idx)
+                    '    item_idx = numerario_mov.descricao.IndexOf(descricao)
+
+                    'End While
+
+                    ' Remover codigo da lista de  cada movimento 
+                    Dim item_idx As Integer
+                    If rdBtnNumerario.Checked Then
+                        item_idx = numerario_mov.codigo.IndexOf(codigo)
+                        While item_idx >= 0
+                            numerario_mov.codigo.RemoveAt(item_idx)
+                            item_idx = numerario_mov.codigo.IndexOf(codigo)
+
+                        End While
+                    ElseIf rdBtnCheques.Checked Then
+                        item_idx = cheque_mov.codigo.IndexOf(codigo)
+                        While item_idx >= 0
+                            cheque_mov.codigo.RemoveAt(item_idx)
+                            item_idx = cheque_mov.codigo.IndexOf(codigo)
+
+                        End While
+                    ElseIf rdBtnPagAutomatico.Checked Then
+                        item_idx = pagamentoAutomatico_mov.codigo.IndexOf(codigo)
+                        While item_idx >= 0
+                            pagamentoAutomatico_mov.codigo.RemoveAt(item_idx)
+                            item_idx = pagamentoAutomatico_mov.codigo.IndexOf(codigo)
+
+                        End While
+                    ElseIf rdBtnOutros.Checked Then
+                        item_idx = outros_mov.codigo.IndexOf(codigo)
+                        While item_idx >= 0
+                            outros_mov.codigo.RemoveAt(item_idx)
+                            item_idx = outros_mov.codigo.IndexOf(codigo)
+
+                        End While
+                    ElseIf rdBtnAbertura.Checked Then
+                        item_idx = abertura_mov.codigo.IndexOf(codigo)
+                        While item_idx >= 0
+                            abertura_mov.codigo.RemoveAt(item_idx)
+                            item_idx = abertura_mov.codigo.IndexOf(codigo)
+
+                        End While
+                    ElseIf rdBtnFecho.Checked Then
+                        item_idx = fecho_mov.codigo.IndexOf(codigo)
+                        While item_idx >= 0
+                            fecho_mov.codigo.RemoveAt(item_idx)
+                            item_idx = fecho_mov.codigo.IndexOf(codigo)
+
+                        End While
+                    ElseIf rdBtnEntrada.Checked Then
+                        item_idx = entrada_mov.codigo.IndexOf(codigo)
+                    While item_idx >= 0
                         entrada_mov.codigo.RemoveAt(item_idx)
+                        item_idx = entrada_mov.codigo.IndexOf(codigo)
+
+                    End While
+                    ElseIf rdBtnSaida.Checked Then
+                        item_idx = saida_mov.codigo.IndexOf(codigo)
+                        While item_idx >= 0
+                            saida_mov.codigo.RemoveAt(item_idx)
+                            item_idx = saida_mov.codigo.IndexOf(codigo)
+
+                        End While
                     End If
-                ElseIf rdBtnSaida.Checked Then
-                    item_idx = saida_mov.codigo.IndexOf(codigo)
-                    If item_idx >= 0 Then
-                        saida_mov.codigo.RemoveAt(item_idx)
-                    End If
+
+
                 End If
-
-            End If
-
         End If
-
     End Sub
 
     Private Sub ConfigMovimentos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -209,87 +263,127 @@ Public Class ConfigMovimentos
 
     Private Sub chkLstMovDescricao_ItemCheck(sender As Object, e As ItemCheckEventArgs) Handles chkLstMovDescricao.ItemCheck
 
-        If Not loading Then
-            Dim i As Integer = Me.chkLstMovDescricao.SelectedIndex()
-            Dim descricao As String
+        ''If Not loading Then
+        '   Dim i As Integer = Me.chkLstMovDescricao.SelectedIndex()
+        Dim i As Integer = e.Index
+        Dim descricao As String
             descricao = Me.chkLstMovDescricao.Items.Item(i).ToString
 
 
             If Not (Me.chkLstMovDescricao.GetItemChecked(i)) Then
-                ' Adicionar codigo que representa cada movimento a uma lista 
+            ' Adicionar codigo que representa cada movimento a uma lista 
 
-                If rdBtnNumerario.Checked Then
+            If rdBtnNumerario.Checked Then
+                If numerario_mov.descricao.IndexOf(descricao) < 0 Then
                     numerario_mov.descricao.Add(descricao)
+                End If
 
-                ElseIf rdBtnCheques.Checked Then
+            ElseIf rdBtnCheques.Checked Then
+                If cheque_mov.descricao.IndexOf(descricao) < 0 Then
+
                     cheque_mov.descricao.Add(descricao)
+                End If
 
+            ElseIf rdBtnPagAutomatico.Checked Then
+                If pagamentoAutomatico_mov.descricao.IndexOf(descricao) < 0 Then
 
-                ElseIf rdBtnPagAutomatico.Checked Then
                     pagamentoAutomatico_mov.descricao.Add(descricao)
+                End If
 
-                ElseIf rdBtnOutros.Checked Then
+            ElseIf rdBtnOutros.Checked Then
+                If outros_mov.descricao.IndexOf(descricao) < 0 Then
+
                     outros_mov.descricao.Add(descricao)
-                ElseIf rdBtnAbertura.Checked Then
+                End If
+            ElseIf rdBtnAbertura.Checked Then
+                If abertura_mov.descricao.IndexOf(descricao) < 0 Then
+
                     abertura_mov.descricao.Add(descricao)
-                ElseIf rdBtnFecho.Checked Then
+                End If
+            ElseIf rdBtnFecho.Checked Then
+                If fecho_mov.descricao.IndexOf(descricao) < 0 Then
+
                     fecho_mov.descricao.Add(descricao)
-                ElseIf rdBtnEntrada.Checked Then
+                End If
+            ElseIf rdBtnEntrada.Checked Then
+                If entrada_mov.descricao.IndexOf(descricao) < 0 Then
+
                     entrada_mov.descricao.Add(descricao)
-                ElseIf rdBtnSaida.Checked Then
+                End If
+            ElseIf rdBtnSaida.Checked Then
+                If saida_mov.descricao.IndexOf(descricao) < 0 Then
+
                     saida_mov.descricao.Add(descricao)
                 End If
+            End If
 
             Else
-                ' Remover codigo da lista de  cada movimento 
-                Dim item_idx As Integer
-                If rdBtnNumerario.Checked Then
+            ' Remover codigo da lista de  cada movimento ,
+            ' 30/10/20
+            ' usar loop para verificar se item ainda existe na lista e remover
+            Dim item_idx As Integer
+            If rdBtnNumerario.Checked Then
+                item_idx = numerario_mov.descricao.IndexOf(descricao)
+
+                While item_idx >= 0
+                    numerario_mov.descricao.RemoveAt(item_idx)
                     item_idx = numerario_mov.descricao.IndexOf(descricao)
-                    If item_idx >= 0 Then
-                        numerario_mov.descricao.RemoveAt(item_idx)
-                    End If
-                ElseIf rdBtnCheques.Checked Then
+
+                End While
+            ElseIf rdBtnCheques.Checked Then
+                item_idx = cheque_mov.descricao.IndexOf(descricao)
+                While item_idx >= 0
+                    cheque_mov.descricao.RemoveAt(item_idx)
                     item_idx = cheque_mov.descricao.IndexOf(descricao)
-                    If item_idx >= 0 Then
-                        cheque_mov.descricao.RemoveAt(item_idx)
-                    End If
-                ElseIf rdBtnPagAutomatico.Checked Then
+            End While
+            ElseIf rdBtnPagAutomatico.Checked Then
+            item_idx = pagamentoAutomatico_mov.descricao.IndexOf(descricao)
+                While item_idx >= 0
+                    pagamentoAutomatico_mov.descricao.RemoveAt(item_idx)
                     item_idx = pagamentoAutomatico_mov.descricao.IndexOf(descricao)
-                    If item_idx >= 0 Then
-                        pagamentoAutomatico_mov.descricao.RemoveAt(item_idx)
-                    End If
-                ElseIf rdBtnOutros.Checked Then
+
+                End While
+            ElseIf rdBtnOutros.Checked Then
+                item_idx = outros_mov.descricao.IndexOf(descricao)
+                While item_idx >= 0
+                    outros_mov.descricao.RemoveAt(item_idx)
                     item_idx = outros_mov.descricao.IndexOf(descricao)
-                    If item_idx >= 0 Then
-                        outros_mov.descricao.RemoveAt(item_idx)
-                    End If
-                ElseIf rdBtnAbertura.Checked Then
+
+                End While
+            ElseIf rdBtnAbertura.Checked Then
+                item_idx = abertura_mov.descricao.IndexOf(descricao)
+                While item_idx >= 0
+                    abertura_mov.descricao.RemoveAt(item_idx)
                     item_idx = abertura_mov.descricao.IndexOf(descricao)
-                    If item_idx >= 0 Then
-                        abertura_mov.descricao.RemoveAt(item_idx)
-                    End If
-                ElseIf rdBtnFecho.Checked Then
+
+                End While
+            ElseIf rdBtnFecho.Checked Then
+                item_idx = fecho_mov.descricao.IndexOf(descricao)
+                While item_idx >= 0
+                    fecho_mov.descricao.RemoveAt(item_idx)
                     item_idx = fecho_mov.descricao.IndexOf(descricao)
-                    If item_idx >= 0 Then
-                        fecho_mov.descricao.RemoveAt(item_idx)
-                    End If
-                ElseIf rdBtnEntrada.Checked Then
+
+                End While
+            ElseIf rdBtnEntrada.Checked Then
+                item_idx = entrada_mov.descricao.IndexOf(descricao)
+                While item_idx >= 0
+                    entrada_mov.descricao.RemoveAt(item_idx)
                     item_idx = entrada_mov.descricao.IndexOf(descricao)
-                    If item_idx >= 0 Then
-                        entrada_mov.descricao.RemoveAt(item_idx)
-                    End If
-                ElseIf rdBtnSaida.Checked Then
+
+                End While
+            ElseIf rdBtnSaida.Checked Then
+                item_idx = saida_mov.descricao.IndexOf(descricao)
+                    While item_idx >= 0
+                    saida_mov.descricao.RemoveAt(item_idx)
                     item_idx = saida_mov.descricao.IndexOf(descricao)
-                    If item_idx >= 0 Then
-                        saida_mov.descricao.RemoveAt(item_idx)
+
+                End While
+
+
+
                     End If
-
-
-
-                End If
 
             End If
-        End If
     End Sub
 
     Private Sub rdBtnNumerario_CheckedChanged(sender As Object, e As EventArgs) Handles rdBtnNumerario.CheckedChanged
@@ -597,7 +691,7 @@ Public Class ConfigMovimentos
     End Sub
 
     Private Sub ConfigMovimentos_Deactivate(sender As Object, e As EventArgs) Handles MyBase.Deactivate
-        carregarConfiguracao()
+        'carregarConfiguracao()
 
     End Sub
 
