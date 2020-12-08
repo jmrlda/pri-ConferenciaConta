e745dcd04545c520085f598ff6a200e123c6e3f7
liry.linx <dercioguirruta44@gmail.com>
2020-12-08 11:52:18 +0200

    #14 - Setup conexão administrador e metodo para obter a conexão no ficheiro configuração

--- pri-fechoConta/jmrJson.vb
+++ pri-fechoConta/jmrJson.vb
@@ -7,12 +7,16 @@ Public Class JmrJson
     Public js As JavaScriptSerializer
 
     Dim conBd As Conexao = New Conexao()
+    Dim conBdAdmin As Conexao = New Conexao()
+
     Dim movDb As Movimentos = New Movimentos()
     Dim utilitario As New Util
 
     ' msgbox("config")
 
     Public CONEXAO_PATH As String = utilitario.dataPathConfig + "\conexaobd.json"
+    Public CONEXAO_PATH_ADMIN As String = utilitario.dataPathConfig + "\conexaobdadmin.json"
+
     Public CLIENTE_PATH As String = utilitario.dataPathConfig + "\cliente.json"
     Public MOVIMENTO_PATH As String = utilitario.dataPathConfig + "\movimento.json"
 
@@ -68,6 +72,19 @@ Public Class JmrJson
         Return New Movimentos()
     End Function
 
+    Public Function getConexaoAdmin() As Conexao
+        js = New JavaScriptSerializer()
+
+        If System.IO.File.Exists(CONEXAO_PATH_ADMIN) Then
+
+
+            Dim jsStr As String = File.ReadAllText(CONEXAO_PATH_ADMIN)
+            conBdAdmin = js.Deserialize(Of Conexao)(jsStr)
+            Return Me.conBdAdmin
+        End If
+
+        Return New Conexao()
+    End Function
 
 
 End Class
