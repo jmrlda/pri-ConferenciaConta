c58e7975ffbc50e26748cef0b1ca672120a22f9c
guirruta <yotlanga@gmail.com>
2022-04-05 09:30:18 +0200

    Atualzar system runtime para 1.12.3

--- pri-fechoConta/My Project/AssemblyInfo.vb
+++ pri-fechoConta/My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.12.1")>
-<Assembly: AssemblyFileVersion("1.12.1")>
+<Assembly: AssemblyVersion("1.12.3")>
+<Assembly: AssemblyFileVersion("1.12.3")>
 <Assembly: NeutralResourcesLanguage("pt")>
